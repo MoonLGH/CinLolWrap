a6a3570572894584d129f26c4dcc75faa12a5712
MoonLGH <MoonLMC1212@gmail.com>
2022-12-24 18:19:09 +0000

    temp

--- src/lib/getHome.ts
+++ src/lib/getHome.ts
@@ -1,22 +1,41 @@
-import axios from "axios";
 import {load} from "cheerio";
 import {endpoints} from "../utils/api";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(StealthPlugin());
+import {Page} from "puppeteer";
+import { executablePath } from "puppeteer";
 
 export async function getHome() {
-  const res = await axios.get(endpoints.baseUrl);
-  const $ = load(res.data);
-  const results:Result[] = [];
-  $("#top-page > div.flex.flex-col > div > div > div > div").each((i, el)=>{
+  const pup = await puppeteer.launch({executablePath:executablePath()});
+  const res = await bypass((await pup.newPage()), endpoints.baseUrl);
+  const $ = load(((await res?.text()) as string));
+  const results: Result[] = [];
+  $("#top-page > div.flex.flex-col > div > div > div > div").each((i, el) => {
     const type = $(el).find("div.styles_container__4elzA > span").text();
-    const books:Book[] = [];
-    $(el).find("div > div > .absolute").each((i, el)=>{
-      const code = ($(el).parent().find("a").attr("href")?.split("/v/")[1] as string);
-      const detail = $(el).parent().find("div[class*=\"detail\"]");
-      const page = $(el).parent().find("div[class*=\"image\"]").find("span > span").text();
-      const title = detail.find("div").text();
-      const lang = detail.find("span > span").text().replace(/[^a-zA-Z]/gm, "");
-      books.push({code, title, lang, page});
-    });
+    const books: Book[] = [];
+    $(el)
+        .find("div > div > .absolute")
+        .each((i, el) => {
+          const code = $(el)
+              .parent()
+              .find("a")
+              .attr("href")
+              ?.split("/v/")[1] as string;
+          const detail = $(el).parent().find("div[class*=\"detail\"]");
+          const page = $(el)
+              .parent()
+              .find("div[class*=\"image\"]")
+              .find("span > span")
+              .text();
+          const title = detail.find("div").text();
+          const lang = detail
+              .find("span > span")
+              .text()
+              .replace(/[^a-zA-Z]/gm, "");
+          books.push({code, title, lang, page});
+        });
+    console.log(type);
     if (type) {
       results.push({type, books});
     }
@@ -24,22 +43,22 @@ export async function getHome() {
   return results;
 }
 
+
+export async function bypass(page:Page, url:string) {
+  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.58");
+  const response = await page.goto(url, {timeout: 30000, waitUntil: "domcontentloaded"});
+  if (response && response.status() === 503) return page.waitForNavigation();
+  return response;
+}
+
 interface Result {
-   type:string
-   books:Book[]
+  type: string;
+  books: Book[];
 }
 
 interface Book {
- code:string,
- title:string,
- lang:string,
- page:string
+  code: string;
+  title: string;
+  lang: string;
+  page: string;
 }
-
-// function substringBefore(str:string, before:string) {
-//   return str.substring(0, str.indexOf(before));
-// }
-
-// function substringAfter(str:string, after:string) {
-//   return str.substring(str.indexOf(after)+after.length, str.length);
-// }
--- test/withoutUvu.ts
+++ test/withoutUvu.ts
@@ -2,5 +2,4 @@ import {getHome} from "../src/index";
 
 getHome().then((res) =>{
   console.log(res);
-  console.log(res[1].books);
 });
